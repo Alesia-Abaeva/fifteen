e3e0ed8be278164efecb45ed33a601b2bf173d81
Alesia <alesia.litvinenko@gmail.com>
2022-10-18 20:37:02 +0300

    feat: change button on click

--- RSS Gem Puzzle/src/js/app.js
+++ RSS Gem Puzzle/src/js/app.js
@@ -33,10 +33,8 @@ function setPositionItems(matrix){
 const shuffleButton = document.getElementById('shuffle')
 
 shuffleButton.onclick = () => {
-    // console.log('13131')
     const flatMatrix = matrix.flat()
     const shuffledArray = shuffleaAray(flatMatrix)
-    console.log(shuffledArray)
     matrix = getMatrix(shuffledArray)
     setPositionItems(matrix)
 }
@@ -47,33 +45,63 @@ function shuffleaAray(array){
 
 // 3. Change position by click
 
+const blankNumber = 16
+
+container.addEventListener('click', (event)=>{
+    const buttonNode = event.target.closest('button')
+  
+    if(!buttonNode){
+        return
+    }
+
+    const buttonNumber = Number(buttonNode.dataset.matrixId)
+    // console.log(buttonNumber)
+    const buttonCoords = findCoordinatesByNumber(buttonNumber, matrix)
+    const  blankCoords =findCoordinatesByNumber(blankNumber, matrix)
+    const isValide = isValidForSwap(buttonCoords, blankCoords)
+
+    if(isValide){
+        swap(blankCoords, buttonCoords, matrix),
+        setPositionItems(matrix)
+    }
+    console.log(isValide)
+
+
+})
+
+function findCoordinatesByNumber(number, matrix){
+    for(let y = 0; y < matrix.length; y++){
+        for(let x = 0; x< matrix[y].length; x++){
+            if(matrix[y][x] === number){
+                 return {x, y}
+            }
+        }
+    }
+    return null
+
+}
+
+function isValidForSwap(coorder1, coorder2){
+    const differentX = Math.abs(coorder1.x - coorder2.x)
+    const differentY = Math.abs(coorder1.y - coorder2.y)
+
+    return (differentX === 1 || differentY === 1) && (coorder1.x === coorder2.x || coorder1.y === coorder2.y)
+
+}
+
+function swap(coorder1, coorder2, matrix){
+    const coords1Number = matrix[coorder1.y][coorder1.x]
+    matrix[coorder1.y][coorder1.x] = matrix[coorder2.y][coorder2.x]
+    matrix[coorder2.y][coorder2.x] = coords1Number
+    // const coords2Number = matrix[coorder1.y][coorder1.x]
+
+}
+
 
 // 4. Change position by keydown
 
 // 4. Show won
 
-// 
-// function getMatrix(array){
-//     const matrix = [[], [], [], []]
-//     let y = 0
-//     let x = 0
-
-//     for(let i = 0; i< array.length; i++){
-//         if(x >= 4){
-//             y++;
-//             x=0
-//         }
-
-//         matrix[y][x] = array[i]
-//         x++
-//     }
-
-//     return matrix
-// }
 
 
 
-// function setNodeStyles(node, x, y){
-//     const shiftPs = 100
-//     node.style.transform = `translate3D(${x * shiftPs}%, ${y * shiftPs}%, 0)`
-// }

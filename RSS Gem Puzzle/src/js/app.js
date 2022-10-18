5aa359a0bb0482afc0fa6a7a15f48ef797de1322
Alesia <alesia.litvinenko@gmail.com>
2022-10-18 15:35:57 +0300

    init: task rss gem puzzle

---
+++ RSS Gem Puzzle/src/js/app.js
@@ -0,0 +1,14 @@
+
+// генерим квадраты
+const puzzleContainer = document.querySelector('.puzzle__container')
+// console.log(puzzleContainer)
+let number = 16
+let valuesPuzzle = new Array(number).fill(0).map((_item, index)=>{
+    return index+1
+})
+
+console.log(valuesPuzzle)
+
+function addValues(){
+
+}
---
+++ RSS Gem Puzzle/webpack.config.js
@@ -0,0 +1,84 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackHarddiskPlugin = require("html-webpack-harddisk-plugin");
+
+// const APP_PAGES = ["main", "donate"];
+
+// const generateMultiplePages = (pages) =>
+//   pages.map(
+//     (name) =>
+//       new HtmlWebpackPlugin({
+//         alwaysWriteToDisk: true,
+//         template: path.join(__dirname, "pages", name, "index.html"),
+//         filename: `${name === pages[0] ? "index" : name}.html`,
+//         chunks: [`${name}`],
+//         favicon: "./src/img/favicon.png",
+//         // clean: true,
+//       })
+//   );
+
+// const generateMultipleJsEntries = (entries) =>
+//   entries.reduce(
+//     (resultEntry, entry) => ({
+//       ...resultEntry,
+//       [entry]: path.join(__dirname, "pages", entry, "script.js"),
+//     }),
+//     {}
+//   );
+
+module.exports = {
+  entry: {
+    index: './src/js/app.js'
+    // main: "./pages/main/script.js",
+    // donate: "./pages/donate/script.js",
+    // ...generateMultipleJsEntries(APP_PAGES),
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+    hot: true,
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.join(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'RSS Gem Puzzle',
+      alwaysWriteToDisk: true,
+      template: path.join(__dirname, "src",  "index.html"),
+      filename: `index.html`,
+      chunks: `index.html`,
+      // favicon: "./src/img/favicon.png",
+        clean: true,
+    }),
+    new HtmlWebpackHarddiskPlugin(),
+    // ...generateMultiplePages(APP_PAGES),
+  ],
+  resolve: {
+    alias: {
+      assets: path.resolve(__dirname, "assets"),
+      src: path.resolve(__dirname, "src"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(html)$/i,
+        use: ["html-loader"],
+      },
+    ],
+  },
+};
